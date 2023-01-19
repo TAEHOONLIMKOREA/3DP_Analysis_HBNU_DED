2d665a531c34dd52e147c98ad671a384263750db
Taehoon <lth9029@gmail.com>
2023-01-19 18:01:06 +0900

    modifing influx_helper

--- main.py
+++ main.py
@@ -1,20 +1,15 @@
 from sources import data_loader
 from sources import influx_helper
 
+
 if __name__ == '__main__':
     dir_path = 'data/20220612_DED.csv'
     # method_1(pandas)
     # data = data_loader.csv_data_load_pandas(dir_path)
-    #
-    # influx_helper.create_db(dbname='HBNU_DED')
-    #
     # influx_helper.data_insert_pandas(data)
 
     #method_2(nomal)
     data = data_loader.csv_data_load(dir_path)
-
-    influx_helper.create_db(dbname='HBNU_DED')
-
     influx_helper.data_insert(data)
 
 
--- sources/influx_helper.py
+++ sources/influx_helper.py
@@ -1,27 +1,25 @@
 from influxdb import InfluxDBClient, DataFrameClient
 
-class info:
-    host = 'keties.iptime.org'
-    port = 55586
-    user = 'honeyb'
-    passwd = '12345'
-    dbname = 'HBNU_DED'
-    protocol = 'line'
-    measurement = '20220612_1454_6'
-    client_pandas = DataFrameClient(host,port, user, passwd, dbname)
-    client = InfluxDBClient(host=host, port=port)
+host = 'keties.iptime.org'
+port = 55586
+user = 'honeyb'
+passwd = '12345'
+protocol = 'line'
+dbname = 'HBNU_DED'
+measurement = '20220612_1454_6'
 
-def create_db(dbname):
-    # check database list
-    list_db = info.client.get_list_database()
-    ret = next((item for item in list_db if item['name'] == dbname), None)
-    if ret is None:
-        info.client.create_database(dbname)
 
 def data_insert_pandas(data):
+    client_pandas = DataFrameClient(host, port, user, passwd, dbname)
     # Write data to measurement of 'HBNU_DED' database.
-    info.client_pandas.write_points(data, info.measurement, tag_columns=['Z'], protocol=info.protocol)
+    client_pandas.write_points(data, measurement, tag_columns=['Z'], protocol=protocol)
 
 def data_insert(data):
+    client = InfluxDBClient(host=host, port=port)
+    # check database list
+    list_db = client.get_list_database()
+    ret = next((item for item in list_db if item['name'] == dbname), None)
+    if ret is None:
+        client.create_database(dbname)
     # Write data to measurement of 'HBNU_DED' database.
-    info.client.write_points(data, database=info.dbname)
+    client.write_points(data, database=dbname)
